fbdd8db9656a883d9e2edb9fc4dc5be687877db5
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-02-29 08:55:51 +0300

    Изменена модель вопроса, для удобной работы с библиотекой OpenCSV

--- homework1/src/main/java/ru/otus/ovodkov/homework/domain/Question.java
+++ homework1/src/main/java/ru/otus/ovodkov/homework/domain/Question.java
@@ -1,16 +1,27 @@
 package ru.otus.ovodkov.homework.domain;
 
+import com.opencsv.bean.CsvBindByPosition;
+
 import java.util.List;
 
 /**
  * Вопрос тестирования
  *
- * @author ovodkov.s
+ * @author Sergey Ovodkov
  */
 public class Question {
 
+    @CsvBindByPosition(position = 0)
     private String question;
-    private List<String> answerOptions;
+    @CsvBindByPosition(position = 1)
+    private String possibleAnswer1;
+    @CsvBindByPosition(position = 2)
+    private String possibleAnswer2;
+    @CsvBindByPosition(position = 3)
+    private String possibleAnswer3;
+    @CsvBindByPosition(position = 4)
+    private String possibleAnswer4;
+    @CsvBindByPosition(position = 5)
     private int correctAnswer;
 
     public String getQuestion() {
@@ -21,12 +32,36 @@ public class Question {
         this.question = question;
     }
 
-    public List<String> getAnswerOptions() {
-        return answerOptions;
+    public String getPossibleAnswer1() {
+        return possibleAnswer1;
     }
 
-    public void setAnswerOptions(List<String> answerOptions) {
-        this.answerOptions = answerOptions;
+    public void setPossibleAnswer1(String possibleAnswer1) {
+        this.possibleAnswer1 = possibleAnswer1;
+    }
+
+    public String getPossibleAnswer2() {
+        return possibleAnswer2;
+    }
+
+    public void setPossibleAnswer2(String possibleAnswer2) {
+        this.possibleAnswer2 = possibleAnswer2;
+    }
+
+    public String getPossibleAnswer3() {
+        return possibleAnswer3;
+    }
+
+    public void setPossibleAnswer3(String possibleAnswer3) {
+        this.possibleAnswer3 = possibleAnswer3;
+    }
+
+    public String getPossibleAnswer4() {
+        return possibleAnswer4;
+    }
+
+    public void setPossibleAnswer4(String possibleAnswer4) {
+        this.possibleAnswer4 = possibleAnswer4;
     }
 
     public int getCorrectAnswer() {
@@ -39,14 +74,28 @@ public class Question {
 
     /**
      * .ctor
-     *
-     * @param question      Тело вопроса
-     * @param answerOptions Список вариантов ответов.
-     * @param correctAnswer Номер правильного варианта ответа.
+     * @see Question#Question(String, String, String, String, String, int)
      */
-    public Question(String question, List<String> answerOptions, int correctAnswer) {
+    public Question() {
+    }
+
+    /**
+     * .ctor
+     *
+     * @param question        Вопрос теста.
+     * @param possibleAnswer1 Первый вариант ответа на вопрос.
+     * @param possibleAnswer2 Второй вариант ответа на вопрос.
+     * @param possibleAnswer3 Третий вариант ответа на вопрос.
+     * @param possibleAnswer4 Четвертый вариант ответа на вопрос.
+     * @param correctAnswer   Номер правильного варианта ответа.
+     * @see Question#Question()
+     */
+    public Question(String question, String possibleAnswer1, String possibleAnswer2, String possibleAnswer3, String possibleAnswer4, int correctAnswer) {
         this.question = question;
-        this.answerOptions = answerOptions;
+        this.possibleAnswer1 = possibleAnswer1;
+        this.possibleAnswer2 = possibleAnswer2;
+        this.possibleAnswer3 = possibleAnswer3;
+        this.possibleAnswer4 = possibleAnswer4;
         this.correctAnswer = correctAnswer;
     }
 }
