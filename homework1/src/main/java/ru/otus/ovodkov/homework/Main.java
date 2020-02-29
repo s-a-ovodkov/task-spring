c42d3fddc72897a90d43314d18942adaf67cb757
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-02-29 09:26:53 +0300

    Основная логика приложения

--- homework1/src/main/java/ru/otus/ovodkov/homework/Main.java
+++ homework1/src/main/java/ru/otus/ovodkov/homework/Main.java
@@ -1,4 +1,19 @@
 package ru.otus.ovodkov.homework;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.ovodkov.homework.domain.TestResult;
+import ru.otus.ovodkov.homework.services.RenderTestResult;
+import ru.otus.ovodkov.homework.services.StudentTestingService;
+
+/**
+ * @author Sergey Ovodkov
+ */
 public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        StudentTestingService studentTestingService = context.getBean(StudentTestingService.class);
+        TestResult testResult = studentTestingService.studentTesting();
+        RenderTestResult renderTestResult = context.getBean(RenderTestResult.class);
+        renderTestResult.showTestResult(testResult);
+    }
 }
