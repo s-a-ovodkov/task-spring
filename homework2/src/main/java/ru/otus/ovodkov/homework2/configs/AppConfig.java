52f98384728d53f7ae8d3392adabdf2abd1f4c42
Оводков Сергей Александрович <s.ovodkov@krasnodar.pro>
2020-03-06 10:56:08 +0300

    В конфигурации добавлены Bean-ы для чтения файла .properties и для локализации и интернационализации

--- homework2/src/main/java/ru/otus/ovodkov/homework2/configs/AppConfig.java
+++ homework2/src/main/java/ru/otus/ovodkov/homework2/configs/AppConfig.java
@@ -1,17 +1,30 @@
 package ru.otus.ovodkov.homework2.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+import java.util.Locale;
+
 /**
+ * Класс конфигурации приложения
+ *
  * @author Sergey Ovodkov
  * @project homework2
  * @created 2020-03-05
  */
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean
     public MessageSource messageSource() {
@@ -21,5 +34,11 @@ public class AppConfig {
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
+    }
+
+    @Bean
+    public Locale useAppLocale(@Value("${app.language}") String language,
+                               @Value("${app.country}") String country){
+        return new Locale(language, country);
     }
 }
