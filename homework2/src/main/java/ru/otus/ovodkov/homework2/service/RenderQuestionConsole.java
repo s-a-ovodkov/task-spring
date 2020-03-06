f619c1e68f7e911d7b31354f48be574ac5df815d
Оводков Сергей Александрович <s.ovodkov@krasnodar.pro>
2020-03-06 10:33:29 +0300

    fix добавлен атрибут

--- homework2/src/main/java/ru/otus/ovodkov/homework2/service/RenderQuestionConsole.java
+++ homework2/src/main/java/ru/otus/ovodkov/homework2/service/RenderQuestionConsole.java
@@ -1,14 +1,15 @@
 package ru.otus.ovodkov.homework2.service;
 
+import org.springframework.stereotype.Service;
 import ru.otus.ovodkov.homework2.domain.Question;
 
 /**
- * @see RenderQuestion
- *
  * @author Sergey Ovodkov
  * @project homework2
  * @created 2020-03-05
+ * @see RenderQuestion
  */
+@Service
 public class RenderQuestionConsole implements RenderQuestion {
     /**
      * @see RenderQuestion#showQuestion(Question)
