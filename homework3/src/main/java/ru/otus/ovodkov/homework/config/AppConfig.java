a9f13eac9c2cefa465b53ac993d20c873aba7ffd
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-03-13 21:44:38 +0300

    Конфигурация MessageSource

---
+++ homework3/src/main/java/ru/otus/ovodkov/homework/config/AppConfig.java
@@ -0,0 +1,20 @@
+package ru.otus.ovodkov.homework.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/i18n/bundle");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+}
