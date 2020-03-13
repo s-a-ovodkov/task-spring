e35aef67b670048434838de03706253e476a5c0d
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-03-13 21:39:30 +0300

    Модель данных результа тестирования

---
+++ homework3/src/main/java/ru/otus/ovodkov/homework/domain/TestResult.java
@@ -0,0 +1,31 @@
+package ru.otus.ovodkov.homework.domain;
+
+/**
+ * Модель данных результата тестирования
+ *
+ * @author Sergey Ovodkov
+ */
+public class TestResult {
+
+    private final Student student;
+    private final int countCorrectAnswers;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public int getCountCorrectAnswers() {
+        return countCorrectAnswers;
+    }
+
+    /**
+     * .ctor
+     *
+     * @param student             Данные тестируемого студента.
+     * @param countCorrectAnswers Количество правильных ответов.
+     */
+    public TestResult(Student student, int countCorrectAnswers) {
+        this.student = student;
+        this.countCorrectAnswers = countCorrectAnswers;
+    }
+}
