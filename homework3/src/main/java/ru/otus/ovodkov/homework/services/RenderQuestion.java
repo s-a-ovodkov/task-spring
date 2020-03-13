04b3144ab85ac26138e37e0e3265126f378ac78f
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-03-13 21:42:26 +0300

    Сервис отображения вопроса

---
+++ homework3/src/main/java/ru/otus/ovodkov/homework/services/RenderQuestion.java
@@ -0,0 +1,17 @@
+package ru.otus.ovodkov.homework.services;
+
+import ru.otus.ovodkov.homework.domain.Question;
+
+/**
+ * Сервис отображения вопроса
+ *
+ * @author Sergey Ovodkov
+ */
+public interface RenderQuestion {
+    /**
+     * Отображение вопроса.
+     *
+     * @param question Вопрос, который необходимо показать студенту.
+     */
+    void showQuestion(Question question);
+}
