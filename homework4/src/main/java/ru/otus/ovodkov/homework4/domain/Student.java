dc8a7d50dddbf6e65f14a023b5c5bc4bdcbce64f
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-03-15 17:19:29 +0300

    Добавлена модель данных тестируемого студента

---
+++ homework4/src/main/java/ru/otus/ovodkov/homework4/domain/Student.java
@@ -0,0 +1,35 @@
+package ru.otus.ovodkov.homework4.domain;
+
+/**
+ * Модель данных тестируемого студента
+ *
+ * @author Sergey Ovodkov
+ */
+public class Student {
+
+    private final String firstName;
+    private final String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * .ctor
+     *
+     * @param firstName Имя студента.
+     * @param lastName  Фамилия студента.
+     */
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
