b72972f303d56ed7a2a17c08ebb0741aad7941f0
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-03-26 21:25:00 +0300

    Добавлен тест модели жанра книги

---
+++ homework5/src/test/java/ru/otus/ovodkov/homework5/domain/GenreTest.java
@@ -0,0 +1,24 @@
+package ru.otus.ovodkov.homework5.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Доменная модель Genre")
+public class GenreTest {
+
+    public static final long ID_GENRE = 1L;
+    public static final String NAME_GENRE = "Java";
+
+    @DisplayName("корректно создается конструктором")
+    @Test
+    void shouldHaveCorrectConstructor() {
+        Genre genre = new Genre(ID_GENRE, NAME_GENRE);
+
+        assertAll(
+                () -> assertEquals(ID_GENRE, genre.getIdGenre()),
+                () -> assertEquals(NAME_GENRE, genre.getNameGenre())
+        );
+    }
+}
