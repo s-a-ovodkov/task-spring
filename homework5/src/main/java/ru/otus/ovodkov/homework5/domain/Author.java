49c40412eaa6637185e4f61d30a2c4d6fb1539d1
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-03-26 21:06:08 +0300

    Добавлена модель описывающая автора книги

---
+++ homework5/src/main/java/ru/otus/ovodkov/homework5/domain/Author.java
@@ -0,0 +1,32 @@
+package ru.otus.ovodkov.homework5.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Доменная модель автор книги
+ *
+ * @author Sergey Ovodkov
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Author {
+    /**
+     * Идентификатор автора книги
+     */
+    private long idAuthor;
+
+    /**
+     * Имя автора
+     */
+    private String firstName;
+
+    /**
+     * Фамилия автора
+     */
+    private String lastName;
+}
