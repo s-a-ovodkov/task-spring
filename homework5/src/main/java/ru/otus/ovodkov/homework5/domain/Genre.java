c03830fff3f3c5bf7e620068f48d57420a89ff86
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-03-26 21:24:37 +0300

    Добавлена модель жанра книги

---
+++ homework5/src/main/java/ru/otus/ovodkov/homework5/domain/Genre.java
@@ -0,0 +1,27 @@
+package ru.otus.ovodkov.homework5.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Доменная модель описывающая жанр книги
+ *
+ * @author Ovodkov Sergey
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Genre {
+    /**
+     * Идентификатор жанра
+     */
+    private long idGenre;
+
+    /**
+     * Название жанра
+     */
+    private String nameGenre;
+}
