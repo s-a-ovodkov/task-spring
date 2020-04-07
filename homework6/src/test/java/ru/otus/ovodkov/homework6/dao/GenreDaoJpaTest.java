cc3bcb190fdeea78eb7aae38003ee792c7bd3a4d
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-04-07 23:41:32 +0300

    Переработан тест DAO жанров книг

--- homework6/src/test/java/ru/otus/ovodkov/homework6/dao/GenreDaoJpaTest.java
+++ homework6/src/test/java/ru/otus/ovodkov/homework6/dao/GenreDaoJpaTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import ru.otus.ovodkov.homework6.domain.Book;
+import ru.otus.ovodkov.homework6.domain.Genre;
 
-import java.util.Set;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("DAO по работе с жанрами")
 @DataJpaTest
@@ -19,20 +22,39 @@ public class GenreDaoJpaTest {
     @Autowired
     private GenreDaoJpa repositoryJpa;
 
+    @Autowired
+    private TestEntityManager em;
+
     private static final long ID_GENRE = 1L;
-    private static final int EXPECTED_BOOKS = 5;
+    private static final int EXPECTED_CONT_GENRES = 2;
 
-    @DisplayName("возвращает книги указаного жанра")
+    @DisplayName("возвращает жанр по идентификатору")
     @Test
-    void showExpectedReturnBooksByIdGenre() {
-        Set<Book> actual = repositoryJpa.getBooksByIdGenre(ID_GENRE);
+    void shouldReturnExpectedGenre() {
+        Optional<Genre> optionalActualGenre = repositoryJpa.getByIdGenre(ID_GENRE);
+        Genre expectedGenre = em.find(Genre.class, ID_GENRE);
 
-        assertThat(actual).isNotNull().hasSize(EXPECTED_BOOKS)
-                .allMatch(x -> !x.getTitleBook().equals(""))
-                .allMatch(x -> x.getEdition() != 0)
-                .allMatch(x -> x.getYearPublishing() != 0)
-                .allMatch(x -> x.getComments() != null && x.getComments().size() > 0)
-                .allMatch(x -> x.getAuthors() != null && x.getAuthors().size() > 0)
-                .allMatch(x -> x.getGenres() != null && x.getGenres().size() > 0);
+        assertThat(optionalActualGenre).isPresent().get()
+                .isEqualToComparingFieldByField(expectedGenre);
+    }
+
+    @DisplayName("удаляет жанр по указаному идентификатору")
+    @Test
+    void shouldDeleteGenre() {
+        repositoryJpa.delete(ID_GENRE);
+        long actual = repositoryJpa.getAllGenre().size();
+
+        assertEquals(EXPECTED_CONT_GENRES - 1, actual);
+    }
+
+    @DisplayName("получаем список всех жанров")
+    @Test
+    void shouldReturnCorrectGenreListWithAllInfo() {
+        List<Genre> actualList = repositoryJpa.getAllGenre();
+
+        assertThat(actualList).isNotNull().hasSize(EXPECTED_CONT_GENRES)
+                .allMatch(x -> !x.getNameGenre().equals(""))
+                .allMatch(x -> !x.getDescriptionGenre().equals(""))
+                .allMatch(x -> x.getBooks() != null && x.getBooks().size() > 0);
     }
 }
