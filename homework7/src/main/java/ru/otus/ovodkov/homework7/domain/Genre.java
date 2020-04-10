c694d25f0392326c6c736fdcdd2607c79cddb47f
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-04-11 01:56:02 +0300

    Добавлена сущность жанра

---
+++ homework7/src/main/java/ru/otus/ovodkov/homework7/domain/Genre.java
@@ -0,0 +1,59 @@
+package ru.otus.ovodkov.homework7.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Доменная модель описывающая жанр книги
+ *
+ * @author Ovodkov Sergey on 10.04.2020
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "Genres")
+@NamedEntityGraph(name = "genreEntityGraph", attributeNodes = {@NamedAttributeNode("books")})
+public class Genre {
+    /**
+     * Идентификатор жанра
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idGenre")
+    private long idGenre;
+
+    /**
+     * Название жанра
+     */
+    @Column(name = "nameGenre")
+    private String nameGenre;
+
+    /**
+     * Описание жанра
+     */
+    @Column(name = "descriptionGenre")
+    private String descriptionGenre;
+
+    /**
+     * Книги данного жанра
+     */
+    @ManyToMany(mappedBy = "genres", fetch = FetchType.LAZY)
+    private Set<Book> books;
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                " idGenre=" + idGenre +
+                ", nameGenre='" + nameGenre + '\'' +
+                ", descriptionGenre='" + descriptionGenre + '\'' +
+                " }";
+    }
+}
