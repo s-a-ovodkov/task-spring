2f1e61a1ee214f2011d08d87d2918d06f9f3f41d
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-04-26 12:50:17 +0300

    Добавлен стартовый класс приложения

---
+++ homework9/src/main/java/ru/otus/ovodkov/homework9/Application.java
@@ -0,0 +1,15 @@
+package ru.otus.ovodkov.homework9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ovodkov Sergey
+ * created on 26.04.2020
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
