ebc77bef24e7f8274b89dbdf9bea4681af70a69e
s.ovodkov <s.ovodkov@krasnodar.pro>
2020-08-11 14:32:40 +0300

    Добавлен документ описывающий комментарий к книге

---
+++ homework8/src/main/java/ru/otus/ovodkov/homework8/document/Comment.java
@@ -0,0 +1,23 @@
+package ru.otus.ovodkov.homework8.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Document(collection = "comments")
+public class Comment {
+
+    @Id
+    private ObjectId id;
+
+    private String comment;
+}
