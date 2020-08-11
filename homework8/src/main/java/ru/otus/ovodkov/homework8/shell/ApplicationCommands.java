c0f01384359bb626683b6ace6e16f52e1667fc45
s.ovodkov <s.ovodkov@krasnodar.pro>
2020-08-11 14:34:55 +0300

    Добавлен сервис с shell командами приложений

---
+++ homework8/src/main/java/ru/otus/ovodkov/homework8/shell/ApplicationCommands.java
@@ -0,0 +1,14 @@
+package ru.otus.ovodkov.homework8.shell;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.standard.ShellComponent;
+
+/**
+ * Shell команды приложения
+ *
+ * @author Ovodkov Sergey on 11.08.2020
+ */
+@RequiredArgsConstructor
+@ShellComponent
+public class ApplicationCommands {
+}
