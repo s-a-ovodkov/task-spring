b72bb5669aa4c9851bea62a9ff413d3c25373146
s.ovodkov <s.ovodkov@krasnodar.pro>
2020-08-12 12:34:43 +0300

    Переработан контроллер для работы с книгами

--- homework10/src/main/java/ru/otus/ovodkov/homework10/controller/BookController.java
+++ homework10/src/main/java/ru/otus/ovodkov/homework10/controller/BookController.java
@@ -2,9 +2,12 @@ package ru.otus.ovodkov.homework10.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.otus.ovodkov.homework10.entity.Book;
 import ru.otus.ovodkov.homework10.service.BookService;
 
@@ -15,27 +18,43 @@ import java.util.List;
  * created on 11.08.2020
  */
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
 
     private final BookService bookService;
 
-    /**
-     * Получения числа книг в библиотеки
-     * @return Число книг в библиотеки
-     */
-    @GetMapping("/count")
-    public ResponseEntity<Integer> getCountBooks(){
+    @GetMapping("/api/books")
+    @ResponseBody
+    public ResponseEntity<Integer> getCountBooks() {
         return ResponseEntity.ok(bookService.getBooks().size());
     }
 
-    /**
-     * Метод получения списка книг хранящихся в библиотеки
-     * @return Список книг в библиотеки
-     */
-    @GetMapping("/")
-    public ResponseEntity<List<Book>> getBooks(){
-        return ResponseEntity.ok(bookService.getBooks());
+    @GetMapping("/books")
+    public String getBooks(Model model) {
+        List<Book> books = bookService.getBooks();
+        model.addAttribute("books", books);
+        return "books";
+    }
+
+    @GetMapping("/book")
+    public String getBook(@RequestParam("idBook") long idBook, Model model) {
+        Book book = bookService.getBookById(idBook);
+        model.addAttribute("book", book);
+        return "book";
+    }
+
+    @PostMapping("/book/edit")
+    public String saveBook(Book book, Model model) {
+        Book bookSaved = bookService.saveBook(book);
+        model.addAttribute("book", bookSaved);
+        return "book";
+    }
+
+    @GetMapping("/book/delete")
+    public String deleteBook(@RequestParam("idBook") long idBook, Model model) {
+        bookService.delete(idBook);
+        List<Book> books = bookService.getBooks();
+        model.addAttribute("books", books);
+        return "books";
     }
 }
