aebefbe041e0a6c47a3fd893d223cdcb711998d8
s.ovodkov <s.ovodkov@krasnodar.pro>
2020-08-12 12:37:07 +0300

    fix

--- homework10/src/main/java/ru/otus/ovodkov/homework10/controller/IndexController.java
+++ homework10/src/main/java/ru/otus/ovodkov/homework10/controller/IndexController.java
@@ -1,6 +1,5 @@
 package ru.otus.ovodkov.homework10.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author Ovodkov Sergey
  * created on 11.08.2020
  */
-@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
