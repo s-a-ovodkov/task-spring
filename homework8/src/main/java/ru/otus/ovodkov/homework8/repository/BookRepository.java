7ef562edacbeb344599938dbdf2c20b2721ab627
s.ovodkov <s.ovodkov@krasnodar.pro>
2020-08-19 15:23:38 +0300

    Добавлен метод поиска книг указанного жанра

--- homework8/src/main/java/ru/otus/ovodkov/homework8/repository/BookRepository.java
+++ homework8/src/main/java/ru/otus/ovodkov/homework8/repository/BookRepository.java
@@ -3,9 +3,17 @@ package ru.otus.ovodkov.homework8.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ru.otus.ovodkov.homework8.document.Book;
 
+import java.util.List;
+
 /**
- *
  * @author Ovodkov Sergey on 11.08.2020
  */
 public interface BookRepository extends MongoRepository<Book, String> {
+
+    /**
+     * Получение книг указанных жанров
+     * @param genres Жанры искомых книг
+     * @return Список книг указанных жанров
+     */
+    List<Book> findByGenresIn(List<String> genres);
 }
